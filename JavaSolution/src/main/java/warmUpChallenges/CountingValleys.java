aa21ad047a897252cf65efabb4d3db5946ca70f3
hardikp-ezdi <hardik.p@ezdi.us>
2019-04-20 05:34:29 +0530

    project

---
+++ JavaSolution/src/main/java/warmUpChallenges/CountingValleys.java
@@ -0,0 +1,29 @@
+package warmUpChallenges;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CountingValleys {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String s = scanner.nextLine();
+
+        int result = 9;
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
